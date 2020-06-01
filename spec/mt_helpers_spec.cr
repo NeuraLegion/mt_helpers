044df73ae0528aa7df5e0c69d54c9189d562be08
Bar Hofesh <bar.hofesh@gmail.com>
2020-06-01 18:04:40 +0300

    MT specs

--- spec/mt_helpers_spec.cr
+++ spec/mt_helpers_spec.cr
@@ -4,6 +4,13 @@ describe MtHelpers do
   # TODO: Write tests
 
   it "works" do
-    false.should eq(true)
+    Synchronized(Array(String)).new
+  end
+
+  it "equals" do
+    array_a = Synchronized(Array(String)).new(["bar"])
+    array_b = Synchronized(Array(String)).new(["bar"])
+
+    (array_a.equals?(array_b) { |x, y| x == y }).should eq(true)
   end
 end
